74a263f1ee30e361e77ed7dc454723a6eecd19f6
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2023-12-27 23:05:14 +0530

    Product Data initialized

---
+++ src/FakerData/index.js
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker';
+
+const NUMBER_OF_BRANDS = 6;
+const NUMBER_OF_PRODUCT = 24;
+
+function getProductDetails(brandName){
+  return{
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price({min: 1000, max: 5000, symbol: 'Rs'}),
+    image: faker.image.urlLoremFlickr({
+      width: 480,
+      height: 640,
+      category: "fashion"
+    }),
+    rating: faker.number.int({
+      min: 0,
+      max: 5
+    }),
+    reviews: faker.number.int({
+      min: 50,
+      max: 1500
+    }),
+    brand: brandName
+  }
+}
+
+const brands = [];
+
+for(let i=0; i<NUMBER_OF_BRANDS; i++){
+  brands.push(faker.company.name());
+}
+
+export const productData = (NUMBER_OF_PRODUCT) => {
+  const products = [];
+
+  for(let j=0; j<NUMBER_OF_PRODUCT; j++){
+    products.push(getProductDetails(brands[j%NUMBER_OF_BRANDS]))
+  }
+  return products;
+}
