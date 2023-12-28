31e4e7320eab8ea7d03143a656e4cc623f2aede1
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2023-12-28 19:55:07 +0530

    Card container with actual data

--- src/Components/CardContainer/index.jsx
+++ src/Components/CardContainer/index.jsx
@@ -1,34 +1,16 @@
+import { useContext } from 'react';
+
 import './CardContainer.css';
 import Card from '../Card'
 
-
+import { ProductContext } from '../../Context/ProductContextProvider';
 
 function CardContainer() {
-    
-    
-const testObj = [
-    {
-        "id": "4b373d94-0539-41ca-8d70-f77f9c98347b",
-        "name": "Small Steel Shoes",
-        "originalPrice": "1317.00",
-        "image": "https://loremflickr.com/480/640/fashion?lock=8037515336351744",
-        "rating": 4,
-        "review": 167,
-        "brand": "Nader LLC"
-    },{
-        "id": "11598a05-779f-4497-a81e-42571123b296",
-        "name": "Unbranded Fresh Tuna",
-        "originalPrice": "4971.00",
-        "image": "https://loremflickr.com/480/640/fashion?lock=3742676826456064",
-        "rating": 4,
-        "review": 259,
-        "brand": "Hills - Jones"
-    }
-]
+    const { productsData } = useContext(ProductContext)
     
   return (
     <div className='card-container'>
-      {testObj.map(element => <Card
+      {productsData.map(element => <Card
         id={element.id}
         key={element.id}
         image={element.image}
